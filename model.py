8853dbd0785b245faec367f79f490e5d3b1a3b86
Matic Stare <maticstare8@gmail.com>
2020-05-12 15:37:10 +0200

    DOdali model za igro vislic

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+import random
+
 STEVILO_DOVOLJENIH_NAPAK = 10
 PRAVILNA_CRKA = "+"
 PONAVLJENA_CRKA = "o"
@@ -7,29 +9,72 @@ ZMAGA = "W"
 PORAZ = "X"
 
 class Igra:
-    def __init__(self):
+    def __init__(self, geslo, crke=[]):
+        self.geslo = geslo.lower()
+
+        self.crke = [z.lower() for z in crke]
         pass
 
     def napacne_crke(self):
-        pass
+        napacne = [c for c in self.crke if c not in self.geslo]
+        return napacne
 
     def pravilne_crke(self):
-        pass
+        pravilne = [c for c in self.crke if c in self.geslo]
+        return pravilne
 
     def stevilo_napak(self):
-        pass
+        return len(self.napacne_crke())
 
     def zmaga(self):
-        pass
+        for c in self.geslo:
+            if c not in self.crke:
+                return False
+        
+        return True
 
     def poraz(self):
-        pass
+        return self.stevilo_napak() > STEVILO_DOVOLJENIH_NAPAK
 
     def pravilni_del_gesla(self):
-        pass
+        pravilni_del = ""
+        for c in self.geslo:
+            if c in self.crke:
+                pravilni_del += c
+            else:
+                pravilni_del += "_"
+        
+        return pravilni_del
 
     def nepravilni_ugibi(self):
-        pass
+        return " ".join(self.napacne_crke())
 
     def ugibaj(self, crka):
-        pass
+        crka = crka.lower()
+
+        if crka in self.crke:
+            return PONAVLJENA_CRKA
+
+        self.crke.append(crka)
+
+        if crka in self.geslo:
+            if self.zmaga():
+                return ZMAGA
+            else:
+                return PRAVILNA_CRKA
+        else:
+            if self.poraz():
+                return PORAZ
+            else:
+                return NAPACNA_CRKA
+
+
+with open("besede.txt", encoding="utf-8") as f:
+    bazen_besed = f.read().split("\n")
+
+def nova_igra():
+    beseda = random.choice(bazen_besed)
+
+    igra = Igra(beseda)
+    
+    return igra
