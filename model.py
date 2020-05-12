ebcdeda5fb589e3b12dc8e865ba2acc948bab215
Matic Stare <maticstare8@gmail.com>
2020-05-12 14:45:54 +0200

    Pripravljena textovna in pythonova datoteka

---
+++ model.py
@@ -0,0 +1,35 @@
+STEVILO_DOVOLJENIH_NAPAK = 10
+PRAVILNA_CRKA = "+"
+PONAVLJENA_CRKA = "o"
+NAPACNA_CRKA = "-"
+
+ZMAGA = "W"
+PORAZ = "X"
+
+class Igra:
+    def __init__(self):
+        pass
+
+    def napacne_crke(self):
+        pass
+
+    def pravilne_crke(self):
+        pass
+
+    def stevilo_napak(self):
+        pass
+
+    def zmaga(self):
+        pass
+
+    def poraz(self):
+        pass
+
+    def pravilni_del_gesla(self):
+        pass
+
+    def nepravilni_ugibi(self):
+        pass
+
+    def ugibaj(self, crka):
+        pass
